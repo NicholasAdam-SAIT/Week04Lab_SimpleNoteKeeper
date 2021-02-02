681202287ae8c87cca3f03233baffca615d6da85
840018 <840018@SAIT225659IT.ACDM.DS.SAIT.CA>
2021-02-02 15:34:01 -0700

    Week04Lab_SimpleNoteKeeper commit#3 Built out the Note bean as required.

--- src/java/models/Note.java
+++ src/java/models/Note.java
@@ -1,86 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package models;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+package models;
 
 /**
  *
  * @author 840018
  */
-public class Note extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Note</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Note at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+public class Note {
+    
+    //private attributes
+    private String title;
+    private String contents;
+    
+    //no arg constructor that initializes both attributes as empty strings
+    public Note()
+    {
+        title = "";
+        contents = "";
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    
+    //two arg constructor: String-> title and String->contents
+    public Note(String title, String contents)
+    {
+        this.title = title;
+        this.contents = contents;
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    
+    public void setTitle(String title)
+    {
+        this.title = title;
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public void setContents(String contents)
+    {
+        this.contents = contents;
+    }
+    
+    public String getContents()
+    {
+        return contents;
+    }
+    
 }
